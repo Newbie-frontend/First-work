12839dfe371ab57a223b38575aa2ca9da4fe6f99
Newbie-frontend <91102196+Newbie-frontend@users.noreply.github.com>
2021-10-30 02:17:34 +0300

    Add files via upload

---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <b-navbar toggleable="md" type="dark" variant="success">
+      <b-navbar-brand href="#" id="rang2">Question & Answer</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="#" disabled
+            >Question {{ numCorrect }}/{{ numTotal }}</b-nav-item
+          >
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["numTotal", "numCorrect"],
+};
+</script>
+<style scoped>
+.navbar {
+  padding-left: 1rem;
+}
+</style>
